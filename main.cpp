dd65a359aa3ca01d1a56b6ec9bf0cb7a5f5c6c60
Vitalrubbish <zxxzdq2006@sjtu.edu.cn>
2025-04-21 23:37:48 +0800

    减小列车BPT的块的大小

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@
 #include "./STLite/map/map.hpp"
 
 BPT<User, 32> userBPT("user");
-BPT<Train, 6> trainBPT("train");
+BPT<Train, 4> trainBPT("train");
 BPT<StationInfo, 48> stationBPT("station");
 BPT<Order, 24> orderBPT("order");
 BPT<PendingOrder, 24> pendingOrderBPT("pendingOrder");
